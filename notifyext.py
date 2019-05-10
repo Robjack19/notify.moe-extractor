c22ecc8bafd0a0b7e6c817caf494eb270e2f95c0
Chris <46964493+Robjack19@users.noreply.github.com>
2019-05-10 13:22:02 +0530

    Add files via upload

--- notifyext.py
+++ notifyext.py
@@ -1,10 +1,11 @@
 #extracting anime list from notify.moe
 
+import xml.etree.cElementTree as ET
 import requests
 import notifyId
 import replace
 import converter
-import builder
+#import builder
 
 username = str(input('Enter your notify.moe username:\n'))
 userId = (notifyId.notifyId(username))
@@ -18,6 +19,7 @@ elif userId is not 'null':
 
 while validity:
     print('Sucess: ')
+    ausername = str(input('Enter your Anilist username: \n'))
     url = 'https://notify.moe/api/animelist/'
 
     nanimeId = []
@@ -43,7 +45,18 @@ while validity:
     #arranging status
     print('Replacing status: ')
     for b in range(noofanime):
-        astatus.append(replace.replace(nstatus[b]))
+        w, c, p, h, d, =  0, 0, 0, 0, 0
+        Id, w ,c ,p ,h ,d = (replace.replace(nstatus[b]))
+
+        aanimeId.append(Id)
+        watching += w 
+        completed += c 
+        planned += p 
+        hold += h 
+        dropped += d 
+
+
+    atotal = watching + completed + planned + hold + dropped
         
     print('Done! ')
     #replacing Id's
@@ -60,5 +73,44 @@ while validity:
     #print(astatus)
     #building .xml file
     print('Now Building: ')
+
+    myanimelist = ET.Element('myanimelist')
+    myinfo = ET.SubElement(myanimelist, 'myinfo')
+    anime = ET.SubElement(myanimelist,'anime')
+
+    ET.SubElement(myinfo, 'user_id').text = ''
+    ET.SubElement(myinfo, 'user_name').text = ausername
+    ET.SubElement(myinfo, 'user_export_type').text = '1'
+    ET.SubElement(myinfo, 'user_total_anime').text = str(atotal)
+    ET.SubElement(myinfo, 'user_total_watching').text = str(watching)
+    ET.SubElement(myinfo, 'user_total_completed').text = str(completed)
+    ET.SubElement(myinfo, 'user_total_onhold').text = str(hold)
+    ET.SubElement(myinfo, 'user_total_dropped').text = str(dropped)
+    ET.SubElement(myinfo, 'user_total_plantowatch').text = str(planned)
+
+    for d in range(noofanime):
+        ET.SubElement(anime, 'series_animedb_id').text = str(aanimeId[d])
+        ET.SubElement(anime, 'series_title').text = ''
+        ET.SubElement(anime, 'series_type').text = ''
+        ET.SubElement(anime, 'series_episodes').text = ''
+        ET.SubElement(anime, 'my_id').text = '0'
+        ET.SubElement(anime, 'my_watched_episodes').text = '0'
+        ET.SubElement(anime, 'my_start_date').text = '0000-00-00'
+        ET.SubElement(anime, 'my_finish_date').text = '0000-00-00'
+        ET.SubElement(anime, 'my_rated').text = ''
+        ET.SubElement(anime, 'my_score').text = '0'
+        ET.SubElement(anime, 'my_dvd').text = ''
+        ET.SubElement(anime, 'my_storage').text = ''
+        ET.SubElement(anime, 'my_status').text = nstatus[d]
+        ET.SubElement(anime, 'my_comments').text = ''
+        ET.SubElement(anime, 'my_times_watched').text = ''
+        ET.SubElement(anime, 'my_rewatch_value').text = ''
+        ET.SubElement(anime, 'my_tags').text = ''
+        ET.SubElement(anime, 'my_rewatching').text = '0'
+        ET.SubElement(anime, 'my_rewatching_ep').text = '0'
+        ET.SubElement(anime, 'update_on_import').text = '0'
+
+    tree = ET.ElementTree(myanimelist)
+    tree.write('test.xml')
 
     break
--- replace.py
+++ replace.py
@@ -1,23 +1,34 @@
 #replacing status
 def replace(nstatus):
     astatus = ''
+    watching =0
+    completed = 0
+    planned = 0
+    hold = 0
+    dropped = 0
    
     if nstatus == 'watching':
         astatus = (str('Watching'))
+        watching =1
 
     elif nstatus == 'completed':       
         astatus = (str('Completed'))
+        completed =1
 
     elif nstatus == 'planned':      
         astatus = (str('Plan to Watch'))
-        
+        planned = 1
+
     elif nstatus == 'hold':     
         astatus = (str('On-Hold'))
+        hold = 1
 
     elif nstatus == 'dropped':    
         astatus = (str('Dropped'))
+        dropped = 1
 
     else:    
         astatus = (str('Plan to Watch'))
+        planned = 1
     
-    return astatus
+    return astatus, watching, completed, planned, hold, dropped
